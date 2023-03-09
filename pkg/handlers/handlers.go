6ffdd9c0a165626ff24216fcea407587ed945658
Flint Ory <flintory@gmail.com>
2023-03-09 07:54:59 -0500

    section 3 lecture 30 done

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,17 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/flintory5/bookings/pkg/render"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.go.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "about.page.go.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
