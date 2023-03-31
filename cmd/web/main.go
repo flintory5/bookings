cfa8ac3238a5b23bb855f50b374e15cfbaa15148
Flint Ory <flintory@gmail.com>
2023-03-31 07:45:14 -0400

    lecture 82 done

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/flintory5/bookings/internal/config"
 	"github.com/flintory5/bookings/internal/handlers"
+	"github.com/flintory5/bookings/internal/helpers"
 	"github.com/flintory5/bookings/internal/models"
 	"github.com/flintory5/bookings/internal/render"
 )
@@ -17,6 +19,8 @@ import (
 const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -43,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -62,6 +72,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
