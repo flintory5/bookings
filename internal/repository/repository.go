7b4d5a465c20e13ffdc61a50571688dfd425c745
Flint Ory <flintory@gmail.com>
2023-04-19 20:26:43 -0400

    midway thru lecture 104

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,10 @@
 package repository
 
+import "github.com/flintory5/bookings/internal/models"
+
 
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
